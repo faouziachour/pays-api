358942fd9bb647b2f5fdabb03d8457ec9901100d
faouziachour <62372927+faouziachour@users.noreply.github.com>
2022-03-28 09:32:16 +0000

    Update universityRoute.js

--- src/routes/universityRoute.js
+++ src/routes/universityRoute.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const getAllUniversities = require('../controllers/universityController')
+const { getAllUniversities, getUniversitiesByCountry } = require('../controllers/universityController')
 const router = express.Router()
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+var bodyParser = require("body-parser");
+router.use(bodyParser.urlencoded({ extended: true }));
+router.get("/universite", function (req, res) {
+    res.render("form");
+});
+
+router.post('/universite', getUniversitiesByCountry)
 
 router.get('/:pays', getAllUniversities);
 module.exports = router
