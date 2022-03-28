10164a3920959f23238ca92fce508f7a749902d9
faouziachour <62372927+faouziachour@users.noreply.github.com>
2022-03-28 09:32:40 +0000

    Update universityController.js

--- src/controllers/universityController.js
+++ src/controllers/universityController.js
@@ -1,23 +1,32 @@
-const universityService = require('../services/universityService')
+const { getAllUniversitiesSer, getUniversitiesByCountrySer } = require('../services/universityService')
 
 const getAllUniversities = async (req, res) => {
-
     try {
 
-        const { data } = await universityService.getAllUniversitiesSer(req.params.pays);
+        const { data } = await getAllUniversitiesSer(req.params.pays);
         res.render("index", {
 
             universities: data,
             pays: req.params.pays
         });
     }
-
-
     catch (ex) {
-        res.status(500).send(ex.data);
-
+        res.status(500).render('error500');
     }
-
 }
 
-module.exports = getAllUniversities;
+const getUniversitiesByCountry = async (req, res) => {
+    try {
+
+        const { data } = await getAllUniversitiesSer(req.body.pays);
+        res.render("index", {
+            universities: data,
+            pays: req.body.pays
+        });
+    }
+    catch (ex) {
+        res.status(500).render('error500');
+    }
+}
+
+module.exports = { getAllUniversities, getUniversitiesByCountry };
